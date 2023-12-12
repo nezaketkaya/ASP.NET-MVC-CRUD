c9312ed31ba16bf72f7b96a968ebbcc8c2476452
Nezaket Kaya <94640744+nezaketkaya@users.noreply.github.com>
2023-12-13 01:56:22 +0300

    Add files via upload

---
+++ Models/AddEmployeeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AspNetMvcCrud.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ Models/Domain/Employee.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace AspNetMvcCrud.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
